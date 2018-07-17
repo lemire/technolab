6afc9a8a76d7a3d669eac592ce3c3c9eef4cb73f
Daniel Lemire <lemire@gmail.com>
2018-07-17 16:47:17 -0400

    Saving...

--- IsRgood/unexpected.r
+++ IsRgood/unexpected.r
@@ -3,6 +3,12 @@ print(1000.4999999999999999<1000.5)
 x <- 4.0e7
 print((8*x) * x + 1 * 1 + (-1) * (-1) + (2*x)*(-4*x))
 printx <- function() {print(x)}
+
+x <- 1
+addonetox <- function() {
+   x <- x + 1
+}
+
 x <- 0
 printx()
 g <- function() {
